9763060b0337081509a19e80e3aa70d6612f117a
Marcos Freitas <marcos.freitas@live.com>
2025-01-22 19:36:52 -0300

    Implementado a função para adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function limparCampoDeEntrada() {
+      nomeDigitado = document.querySelector('input');
+      nomeDigitado.value = '';
+  }
+
+function adicionarAmigo() {
+      
+      let nomeDigitado = document.querySelector('input').value.trim(); // Remove espaços no início e fim
+      if (nomeDigitado !== "") {
+          amigos.push(nomeDigitado); // Adiciona o nome ao array
+          limparCampoDeEntrada();
+          
+          console.log(nomeDigitado); // Mostra o nome no console
+      } else {
+          alert("Por favor, insira um nome."); // Exibe o alerta se o campo estiver vazio
+      }
+  }
+
+ 
+
